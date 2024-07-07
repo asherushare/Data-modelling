acfbfac3a4106d20fbee4bd54f675ffa03d142dc
asherushare <102591952+asherushare@users.noreply.github.com>
2024-07-07 13:22:11 +0530

    Initial commit

---
+++ models/todos/user.models.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  // username: String,
+  // email: String,
+  // isActive: Boolean
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: [true, "password must be required"]
+  }
+}, {timestamps: true})
+
+export const User = mongoose.model("User", userSchema);
